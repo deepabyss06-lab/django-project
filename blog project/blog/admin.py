b2f274f8a13d2675476541d95eaf08f6d6f446ed
remasteredgod <ap@gmail.com>
2026-03-05 09:12:06 +0530

    admin changes

--- blog project/blog/admin.py
+++ blog project/blog/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from blog.models import Post
 
-admin.site.register(Post)
+#admin.site.register(Post)
+#admin.site.register(Post.author)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+ list_display = ['title', 'slug', 'author', 'publish', 'status']
